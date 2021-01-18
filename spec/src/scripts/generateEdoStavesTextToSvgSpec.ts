604c216ea3f4df10a0bf4d538768f751be98a993
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-18 15:47:12 -0800

    remove vectorize-text version

--- spec/src/scripts/generateEdoStavesTextToSvgSpec.ts
+++ spec/src/scripts/generateEdoStavesTextToSvgSpec.ts
@@ -5,7 +5,7 @@ describe("generate-edo-staves-text-to-svg", (): void => {
     xit("generates a set of staves for each EDO showing its notation in both Evo and Revo flavors, and a chromatic scale plus alterations of a single nominal", (): void => {
         onlyRunInCi()
 
-        const actual = runScriptAndGetConsoleOutput("npm run generate-edo-staves-vectorize-text-text-to-svg")
+        const actual = runScriptAndGetConsoleOutput("npm run generate-edo-staves-text-to-svg")
 
         const expected = [] as Io[]
         expect(actual).toEqual(expected)
--- src/scripts/generateEdoStavesTextToSvg.ts
+++ src/scripts/generateEdoStavesTextToSvg.ts
@@ -1,9 +1,10 @@
-import {Io, Sentence, textToSvg} from "@sagittal/general"
+import {FontName, Io, Sentence, textToSvg} from "@sagittal/general"
 import * as fs from "fs"
 import {computeInputSentenceUnicode} from "staff-code"
-// @ts-ignore
-import TextToSVG from "staff-code-text-to-svg"
 
+const font = "./node_modules/staff-code/dist/package/assets/fonts/BravuraTextBB.otf" as FontName
+
+// TODO: When it gets to the finesse stage, Dave says there should be ~7; between the notes.
 const inputSentence = `
 ston Gcl ;
 c4 nt ; /| ; nt ;
@@ -15,19 +16,10 @@ c5 \\! ; nt ; nt ;
 `
 const unicodeSentence = computeInputSentenceUnicode(inputSentence as Io & Sentence)
 
-const doThing = async (): Promise<void> => {
-    const svgString = await textToSvg(unicodeSentence, {font: "./node_modules/staff-code/dist/package/assets/fonts/BravuraTextBB.otf"})
+const asyncGenerateEdoStaves = async (): Promise<void> => {
+    const svgString = await textToSvg(unicodeSentence, {font})
 
-    // TODO: maybe standardize between the two methods whether they return an SVG or SVG string
-    //  But probably just clean up vectorize text when it's done
     fs.writeFileSync("dist/edoStaves.svg", svgString)
 }
 
-doThing()
-
-// TODO: For whatever reason, this svgString's height is just = 72, which is dumb. That's just the font size.
-//  But it extends beyond that. The solution is to set y to 50 so the "top" baseline is low enough that stuff that
-//  Sticks out the top is still visible. Then, to fix the stuff sticking off the edge the bottom, manually, it's a quick
-//  Fix. You just open the file and modify the SVG's height to be bigger. But programmatically, you'd need to write a
-//  Method here that would go into the SVG string and modify the height.
-
+asyncGenerateEdoStaves().then()
