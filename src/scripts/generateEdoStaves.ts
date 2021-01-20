453d040a9ad92dc9967909e1d790ef958be6011c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-19 16:32:17 -0800

    better version of @sagittal/general means multi-line SVGs are supported

--- src/scripts/generateEdoStaves.ts
+++ src/scripts/generateEdoStaves.ts
@@ -1,18 +1,41 @@
-import {FontName, Io, Sentence, textToSvg} from "@sagittal/general"
+import {Filename, Io, Sentence, textToSvg} from "@sagittal/general"
 import * as fs from "fs"
 import {computeInputSentenceUnicode} from "staff-code"
 
-const font = "./node_modules/staff-code/dist/package/assets/fonts/BravuraTextBB.otf" as FontName
+const font = "./node_modules/staff-code/dist/package/assets/fonts/BravuraTextBB.otf" as Filename
 
 // TODO: When it gets to the finesse stage, Dave says there should be ~7; between the notes.
 const inputSentence = `
-ston Gcl ;
-c4 nt ; /| ; nt ;
+ston
+
+Gcl ;
+c4 nt ; /| ; nt ; 
+bl ;
 d4 \\! ; nt ; nt ; /| ; nt ;
+bl ;
 f4 \\! ; nt ; nt ; /| ; nt ;
+bl ;
 g4 \\! ; nt ; nt ; /| ; nt ;
+bl ;
 a4 \\! ; nt ; nt ; /| ; nt ;
+bl ;
 c5 \\! ; nt ; nt ;
+en; bl
+br;
+
+Gcl ;
+c4 nt ; /| ; nt ;
+bl ;
+d4 \\! ; nt ; nt ; /| ; nt ;
+bl ;
+f4 \\! ; nt ; nt ; /| ; nt ;
+bl ;
+g4 \\! ; nt ; nt ; /| ; nt ;
+bl ;
+a4 \\! ; nt ; nt ; /| ; nt ;
+bl ;
+c5 \\! ; nt ; nt ;
+en; bl
 `
 const unicodeSentence = computeInputSentenceUnicode(inputSentence as Io & Sentence)
 
