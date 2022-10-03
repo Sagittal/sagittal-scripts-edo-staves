5f85ba4beb079bff82a3b2c208ed0edd08cfa884
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2022-10-03 14:14:09 -0700

    no ci

--- spec/src/scripts/generateEdoStavesSpec.ts
+++ spec/src/scripts/generateEdoStavesSpec.ts
@@ -1,8 +1,8 @@
-import {Io, onlyRunInCi, runScriptAndGetConsoleOutput} from "@sagittal/general"
+import {Io, slowTestOnlyRunInFullSuite, runScriptAndGetConsoleOutput} from "@sagittal/general"
 
 describe("generate-edo-staves-text-to-svg", (): void => {
     xit("generates a set of staves for each EDO showing its notation in both Evo and Revo flavors, and a chromatic scale plus alterations of a single nominal", (): void => {
-        onlyRunInCi()
+        slowTestOnlyRunInFullSuite()
 
         const actual = runScriptAndGetConsoleOutput("npm run generate-edo-staves-text-to-svg")
 
