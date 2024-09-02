25797f1b811ee8c33d8ed090f4405f43ace7f84a
Douglas Blumeyer <douglas@mechanical-orchard.com>
2024-09-02 16:19:59 -0700

    proof of concept for manipulating the SVG after the fact

--- src/diagram.ts
+++ src/diagram.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs"
 import { computeInputSentenceUnicode } from "staff-code"
+// import { DOMParser, XMLSerializer } from "xmldom"
 import { Filename, Io, Sentence, textToSvg, Unicode } from "@sagittal/general"
 import { Edo, Nominal, Flavor } from "@sagittal/system"
 import { computeStaffCodeInputSentence } from "./inputSentence"
@@ -15,6 +16,18 @@ const asyncGenerateDiagram = async ({ edo, flavor, root }: { edo: Edo, flavor: F
     const unicodeSentence: Unicode & Sentence = computeInputSentenceUnicode(inputSentence)
 
     const svgString: string = await textToSvg(unicodeSentence, { font })
+
+    // const parser = new DOMParser();
+    // const svgDoc = parser.parseFromString(svgString, "image/svg+xml");
+    // const svg = svgDoc.getElementsByTagName("svg")[0]
+    // if (!svg) return
+    // svg.setAttribute("height", `${(parseInt(svg.getAttribute("height") || "0") * 2)}`)
+    // svg.setAttribute("width", `${(parseInt(svg.getAttribute("width") || "0") * 2)}`)
+    // const g = svgDoc.getElementsByTagName("g")[0]
+    // g?.setAttribute("transform", "translate(50 0)")
+    // const serializer = new XMLSerializer()
+    // const modifiedSvgString = serializer.serializeToString(svgDoc)
+
     if (!fs.existsSync("dist")) fs.mkdirSync("dist")
     fs.writeFileSync(`dist/${edo}-EDO-${flavor}.svg`, svgString)
 }
