c7c9590394eb96c0610bd5b0038bb690170c259e
Douglas Blumeyer <douglas@mechanical-orchard.com>
2024-09-10 13:32:06 -0700

    add a little more spacing to avoid ledger lines

--- src/sentence/assembly/note.ts
+++ src/sentence/assembly/note.ts
@@ -4,13 +4,14 @@ import { Clause, Word } from "@sagittal/general"
 import { AssemblyState } from "./types"
 
 const NOTE_WIDTH: Octals = computeCodewordWidth("ntqrup" as Code & Word) // "nt" is only an alias. the width is 13
+const DO_NOT_TOUCH_LEDGER_LINE_SPACING: Octals = 2 as Octals
 
 const computeNoteAndRighthandSpaceClause = ({ assemblyState, subsetExcluded }: { assemblyState: AssemblyState, subsetExcluded: boolean }): Code & Clause => {
     assemblyState.noteCount++
 
     return subsetExcluded ?
-        `${NOTE_WIDTH}; ${COLUMN_BUFFER_WIDTH}; ` as Code & Clause :
-        `nt; ${COLUMN_BUFFER_WIDTH}; ` as Code & Clause
+        `${DO_NOT_TOUCH_LEDGER_LINE_SPACING}; ${NOTE_WIDTH}; ${COLUMN_BUFFER_WIDTH}; ` as Code & Clause :
+        `${DO_NOT_TOUCH_LEDGER_LINE_SPACING}; nt; ${COLUMN_BUFFER_WIDTH}; ` as Code & Clause
 }
 
 export {
