9c79cdba096a971efce893311249cecb4c59f4b5
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2024-09-12 21:00:33 -0700

    worth considering, especially if this script turns out to have use beyond this project

--- src/scripts/generateEdoStaves.ts
+++ src/scripts/generateEdoStaves.ts
@@ -1,6 +1,6 @@
 import { program } from "commander"
 import { Edo, Flavor } from "@sagittal/system"
-import { Io, Sentence } from "@sagittal/general"
+import { Io, isUndefined, Sentence } from "@sagittal/general"
 import { computeDefaultSingleSpellingPerStepNotationAsStaffCodeInputSentence } from "../sentence"
 import { generateOneOffDiagram } from "../diagram"
 
@@ -10,6 +10,12 @@ program
 
 program.parse()
 const { edo: edoString, flavor: flavorString }: { edo: string, flavor: string } = program.opts()
+
+if (isUndefined(edoString)) throw new Error ("You must provide an EDO.")
+if (isUndefined(flavorString)) throw new Error("You must choose a flavor (Evo, Revo, or Evo-SZ).")
+// TODO: actually could consider letting them leave it off, generating all, 
+// and seeing if they're all the same, sort of inverting all the logic from generate-all
+
 const edo: Edo = parseInt(edoString) as Edo
 const flavor: Flavor = flavorString.toLowerCase() as Flavor
 
