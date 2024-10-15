a2518562056ac99f5d5ef088401cd2a0a15a967d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2024-10-14 20:29:23 -0700

    include up to 77-EDO for main stuff, since those are on the PT now

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { Max } from "@sagittal/general"
 import { Edo } from "@sagittal/system"
 
-const MAX_PERIODIC_TABLE_EDO: Max<Edo> = 72 as Max<Edo>
+const MAX_PERIODIC_TABLE_EDO: Max<Edo> = 77 as Max<Edo>
 
 export { MAX_PERIODIC_TABLE_EDO }
